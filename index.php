c1d76273899694167d490b639a491c32ba376b88
Ornelio Reiph <44802153+oreiph28@users.noreply.github.com>
2018-12-10 14:44:35 +0100

    marktplaats
    weekopdracht
    front end

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="generator" content="CoffeeCup HTML Editor (www.coffeecup.com)">
+    <meta name="dcterms.created" content="ma, 10 dec 2018 08:30:02 GMT">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+	
+	<link rel="stylesheet" href="css/index.css" />
+	<link rel="stylesheet" href="css/navigation" />
+    <title>Virtual Market</title>
+    
+
+  </head>
+  <body>
+  <br />
+ 
+ 
+  <div class="menu">
+	  <div class="menu1">
+	  <input type="button" value="Virtual Market" ></input>
+         <a href="https://www.marktplaats.nl/">Virtual Market</a>
+         <a href="#">Help en info</a>
+         <a href="#">Voorwaarden</a>
+         <a href="#">Veilig handelen</a>
+	  </div>
+	  
+	  <div class="menu2">
+	     <a href="#">Berichten</a>
+	     <a href="#">Meldingen</a>
+	     <a href="#">Inloggen</a> 
+		 <a href="about.php">About</a>
+	  <input type="button" value="Plaats advertentie"/>
+	  </div>
+	
+ </div>
+	
+	<br />
+	<br />
+	
+	
+	<!-- =========================================================================
+	================================Search Menu===================================
+	========================================================================== -->
+<div class="header">
+   
+ <div class="Searchmenu" align="center">
+<input class="searchInput" type="text" name="searchInput" placeholder="Search" required>
+
+<select class="searchSelect" name="searchSelect" align="top">
+   <option selected value="1">Alle groepen</option>
+   <option value="2">Antiek en Kunst</option>
+   <option value="2">Adio, TV, Foto's</option>
+   <option value="2">Auto's</option>
+   <option value="3">auto-onderdelen</option>
+   <option value="2">auto-diversen</option>
+   <option value="4">Boeken</option>
+   <option value="2">Caravans en Kamperen</option>
+   <option value="5">Cd's en Dvd'S</option>
+   <option value="2">Computers en Software</option>
+   <option value="6">Contacten en Berichten</option>
+   <option value="2"d>Diensten en Vakmensen</option>
+   <option value="7">Dieren en Toebehoren</option>
+   <option value="2">Doe-Het-Zelfen Verbouw</option>
+   <option value="8">Fietsen en Brommers</option>
+   <option value="2">Hobby en Vrije tijd</option>
+   <option value="9">Huis en Inrichting</option>
+   <option value="2">Antiek</option>
+   <option value="10">Huizen en Kamers</option>
+   <option value="2">Kinderen en Baby's</option>
+   <option value="11">Kleding Dames</option>
+   <option value="2">Kleding Heren</option>
+   <option value="12">Motoren</option>
+   <option value="13">Muziek en Instrumenten</option>
+</select>
+
+<input class="PosctcodeInput" type="text" name="searchInput" placeholder="Posctode" required>
+
+<select name="Afstand" class="afstand">
+  <option selected value="0">Alle Afstanden</option>
+  <option value="1">3KM</option>
+  <option value="2">5KM</option>
+  <option value="3">10KM</option>
+  <option value="4">15KM</option>
+  <option value="5">25KM</option>
+  <option value="6">50KM</option>
+  <option value="7">75KM</option>
+</select>
+
+  <input type="button" value="ZOEK" class="btnZoek">
+ </div>
+ </div>
+
+  <br />
+  <br />
+  <br />
+  <br />
+  <br />
+
+ 
+
+ <div class="body">
+  
+   <div class="uitgelicht">
+<a href="#">Antiek en Kunst</a><br />
+<a href="#">Adio, TV, Foto's</a><br />
+<a href="#">Auto's</a><br />
+<a href="#">auto-onderdelen</a><br />
+<a href="#">auto-diversen</a><br />
+<a href="#">Boeken</a><br />
+<a href="#">Caravans en Kamperen</a><br />
+<a href="#">Cd's en Dvd'S</a><br />
+<a href="#">Computers en Software</a><br />
+<a href="#">Contacten en Berichten</a><br />
+<a href="#">Diensten en Vakmensen</a><br />
+<a href="#">Dieren en Toebehoren</a><br />
+<a href="#">Doe-Het-Zelfen Verbouw</a><br />
+<a href="#">Fietsen en Brommers</a><br />
+<a href="#">Huis en Inrichting</a><br />
+<a href="#">Antiek <a href="#">Huizen en Kamers</a><br />
+<a href="#">Kinderen en Baby's</a><br />
+<a href="#">Kleding Dames</a><br />
+<a href="#">Kleding Heren</a><br />
+<a href="#">Motoren</a><br />
+<a href="#">Muziek en Instrumenten</a><br />
+    </div>
+ <br />
+   <div class="footer">
+  jgkjhjjkljlk
+  </div>
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
+
+  </div>
+  </body>
+</html>
